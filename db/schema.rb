898cced25f8a0da05db257760c7737d5b975aff3
koomekevo <koome.kevo@gurucrafts.net>
2022-12-02 11:13:57 +0300

    Run cards table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_074546) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_080357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.bigint "list_id", null: false
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -21,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_074546) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cards", "lists"
 end
