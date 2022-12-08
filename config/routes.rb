0f6436b9272a4a8bd3d1edf009205216e84d92b2
koomekevo <koome.kevo@gurucrafts.net>
2022-12-08 12:36:20 +0300

    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :lists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists
+  resources :cards
+
+  root to: "lists#index"
+
 end
